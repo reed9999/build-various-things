3594433d049c489e2d61292394522b3bd5083614
Philip J. Reed <reed9999@uw.edu>
2023-04-08 02:56:22 +0000

    reflect pytest semi-broken; suspect inappropriate sudo

--- ec2/python-various/pytest-build.sh
+++ ec2/python-various/pytest-build.sh
@@ -1,17 +1,35 @@
+## See ../../output/pytest-summary-on-003.txt
+# The sudo below here is suspect; could it be that the 2nd time through it has already claimed dirs 
+# inappropriately for root?
+
+
 pushd ~/pytest
+python3 -m venv ~/.virtualenvs/pytestdev
+source ~/.virtualenvs/pytestdev/bin/activate
+python3 -m pip install --editable .
+
 echo "-- next: setup.py build"
 python3 ./setup.py build
-echo "-- next: setup.py test"
-# Note that pytest clearly says to use tox instead, and doesn't run any tests.
-python3 ./setup.py test
+echo
+
+
+# Note that pytest clearly says to use tox instead, and the following doesn't run any tests.
+# python3 ./setup.py test
+python3 -m pip install tox
+tox
 echo "-- next: attempt install of our new libraries"
+#
+# I don't know why I thought this was a good idea.
 # python3 -m pip install --upgrade $(pwd)
-sudo python3 ./setup.py install
+
+# This sudo might be problematic.
+# sudo python3 ./setup.py install
+python3 ./setup.py install
 
 popd
 
 # Here are the warnings... even though setup seems to complete.  
-OUTPUT = <<-HERE
+"
 /usr/lib/python3.9/site-packages/setuptools/installer.py:27: SetuptoolsDeprecationWarning: setuptools.installer is deprecated. Requirements should be satisfied by a PEP 517 installer.
   warnings.warn(
 WARNING: The wheel package is not available.
@@ -23,4 +41,4 @@ usage: setup.py [global_opts] cmd1 [cmd1_opts] [cmd2 [cmd2_opts] ...]
    or: setup.py --help-commands
    or: setup.py cmd --help
 
-HERE
+"
