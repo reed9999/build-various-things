86ba14bf94dda655a5b2d695a9ef20292158a31c
Philip J. Reed <reed9999@uw.edu>
2023-04-08 18:47:27 +0000

    perf test ohio010-m6a.4xlarge

--- perf-test-fun.sh
+++ perf-test-fun.sh
@@ -4,28 +4,29 @@ mkdir this-instance
 
 echo
 echo "~~ begin various setup ~~ $(date)"
-echo 
-ec2/python-various/setup.sh
+echo
 
 echo
 echo "~~ begin django build ~~ $(date)"
-echo 
-ec2/python-various/django-build.sh
+echo
+ec2/python-various/django-build.sh | tee this-instance/django-build.txt
 
 echo
 echo "~~ begin pytest build ~~ $(date)"
-echo 
-ec2/python-various/pytest-build.sh
+echo
+ec2/python-various/pytest-build.sh | tee this-instance/pytest-build.txt
 
 echo
 echo "~~ begin python setup ~~ $(date)"
-echo 
-ec2/python/setup.sh
+echo
+ec2/python/setup.sh | tee this-instance/python-setup.txt
 
 echo
 echo "~~ begin python build ~~ $(date)"
-echo 
-ec2/python/build.sh
+echo
+ec2/python/build.sh | tee this-instance/python-build.txt
 
 echo
 echo "~~ end pytest build ~~ $(date)"
+
+popd
