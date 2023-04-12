58815d16f82bf44672e90e4fc2b145921d1154f4
Philip J. Reed <reed9999@uw.edu>
2023-04-12 17:03:33 +0000

    ohio021: successful ruby build and test

--- ec2/ruby/setup.sh
+++ ec2/ruby/setup.sh
@@ -1,20 +1,16 @@
 # https://www.ruby-lang.org/en/documentation/installation/#building-from-source
 # https://github.com/ruby/ruby/blob/master/doc/contributing/building_ruby.md
 #
-# Some of these are unneeded but added to try to fix issues -- try to make this more parsimonious.
+# Some of these may still be unneeded -- be mindful of parsimony
 pushd ~
 sudo yum install -y gcc
 sudo yum install -y libyaml-devel
-# sudo yum install -y zlib
 sudo yum install -y zlib-devel
 sudo yum install -y autoconf
 sudo yum install -y bison
 sudo yum install -y gperf
-# sudo yum install -y libffi
 sudo yum install -y libffi-devel
-# sudo yum install -y openssl # doubtful
 sudo yum install -y openssl-devel
-# sudo yum install -y readline
 sudo yum install -y readline-devel
 sudo yum install -y ruby	# according to official ruby instructions
 sudo yum install -y ruby-devel	# or is it this? or both?
@@ -28,7 +24,8 @@ git clone --depth 1 https://github.com/ruby/ruby.git
 popd
 
 
-# Although it seemed like a longshot this actually made everything but psych appear to work.
+# Although it seemed like a longshot this actually made everything but psych appear to work.
+# However, we don't have rvm installed and it doesn't seem to matter too much.
 #
 # https://github.com/rvm/rvm/issues/5069
 export optflags="-Wno-error=implicit-function-declaration"; \
