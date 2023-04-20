73bdf2eb848af62392d489e69fb77872674e1d19
Philip J. Reed <reed9999@uw.edu>
2023-04-20 17:29:57 +0000

    Dagster build working under nohup.
    tests still running - the output is the nohup.out checkpointed as of commit time

--- azure-vm/python-various/dagster-build.sh
+++ azure-vm/python-various/dagster-build.sh
@@ -1,24 +1,35 @@
+#!/bin/bash
 # On Ubuntu ec2, not Azure
 # Remember I also have a Mac version of this on my work Mac.
-pushd ~
+
+# I don't get why pushd isn't working under nohup
+cd ~
+sudo apt update         # critical
 sudo apt install -y python3.10-venv
 sudo apt install -y python3-pip
+sudo apt install -y nodejs
+sudo apt install -y yarn
 
+rm -rf ~/.virtualenvs/dagsterdev
 python3 -m venv ~/.virtualenvs/dagsterdev
 
 source ~/.virtualenvs/dagsterdev/bin/activate
 echo "post-activate. which python? $(which python3)"
 
     echo "Is node installed? $(node -v)"
-    sudo apt install yarn
+    echo
+    echo
+
     # I should really set up my ssh, but I haven't yet.
     # git clone git@github.com:dagster-io/dagster.git
     git clone --depth=100 https://github.com/dagster-io/dagster.git
-        pushd dagster
-        echo "after push to dagster pwd is $(pwd)"
+        cd ~/dagster
+        echo "after cd to dagster pwd is $(pwd) at $(date)"
         python3 -m pip install --editable .
 	echo "just before make, our python lives at $(which python3)"
 	echo "just before make, $(date)"
+    echo
+    echo
         make dev_install
 	echo "just after make, $(date)"
 
