6ae159051507b6ba1e3dadfbaaac1991ccc5550b
Philip J. Reed <philip.reed@icf.com>
2023-08-02 17:45:45 -0400

    refactor iac

--- iac/main.tf
+++ iac/main.tf
@@ -27,49 +27,70 @@ data "template_file" "bootstrap" {
 }
 
 # INSTANCES
-# These are all hardcoded for now... 
-# Presently focusing on Ubuntu but can switch over to Amazon Linux if desired.
-resource "aws_spot_instance_request" "ohio-small" {
-	count         = var.quantities.USE2.small
-	ami           = var.USE2.amis.ubuntu
-	instance_type = var.instance_types.small
-	spot_price    = "0.04"
-	key_name      = var.USE2.key_name
+# These are all hardcoded for now.
+# There may be some fancy way to iterate with for_each, but we need to figure out how to
+# make the alternative providers for other regions work. Perhaps it would be better to do one of these
+# per region.
+# Also note that for_each prevents use of count.
 
-user_data = file("../ubuntu/bootstrap.sh")
+resource "aws_spot_instance_request" "ohio-micro-amazon" {
+  count         = var.quantities.USE2.micro.amazon
+  ami           = var.config.USE2.amis.amazon_linux
+  user_data = file("../ec2/bootstrap.sh")
+  instance_type = var.instance_types.micro
+  spot_price    = "0.04"
+  key_name      = var.config.USE2.key_name
+
+  # This has no observable effect in spot.
+  tags = {
+    Name = "ohio-micro-amazon"
+
+  }
+}
+
+resource "aws_spot_instance_request" "ohio-micro-ubuntu" {
+	count         = var.quantities.USE2.micro.ubuntu
+    ami           = var.config.USE2.amis.ubuntu_amd
+    user_data = file("../ubuntu/bootstrap.sh")
+	instance_type = var.instance_types.micro
+	spot_price    = "0.04"
+	key_name      = var.config.USE2.key_name
+
 # This has no observable effect in spot.
-tags = {
-  Name = "ohio-small"
+  tags = {
+    Name = "ohio-micro-ubuntu"
 
   }
 }
 
 resource "aws_spot_instance_request" "ohio-large" {
+# Why is this not observed??
   count         = var.quantities.USE2.large
-  # ami           = var.USE2.amis.ubuntu
-  ami           = var.USE2.amis.amazon_linux
-  instance_type = var.instance_types.large
-  spot_price    = "0.10"
-  key_name      = var.USE2.key_name
 
-  user_data = file("../ubuntu/bootstrap.sh")
-  # user_data = file("../ec2/bootstrap.sh")
+#  ami           = var.config.USE2.amis.ubuntu_amd
+#  user_data = file("../ubuntu/bootstrap.sh")
+  ami           = var.config.USE2.amis.amazon_linux
+  user_data = file("../ec2/bootstrap.sh")
+
+  instance_type = var.instance_types.large
+  spot_price    = "0.08"
+  key_name      = var.config.USE2.key_name
+
   tags = {
     Name = "ohio-large"
   }
 }
 
-resource "aws_spot_instance_request" "virginia-small" {
+resource "aws_spot_instance_request" "virginia-micro" {
   provider = aws.aws_USE1
-  count    = var.quantities.USE1.small
-  # us-east-1 doesn't recognize its own stated AMI for Ubuntu. Why not?
-  ami           = var.USE1.amis.ubuntu
-  instance_type = var.instance_types.small
+  count    = var.quantities.USE1.micro
+  ami           = var.config.USE1.amis.ubuntu_amd
+  instance_type = var.instance_types.micro
   spot_price    = "0.04"
-  key_name      = var.USE1.key_name
+  key_name      = var.config.USE1.key_name
   user_data = file("startup.sh")
   tags = {
-    Name = "virginia-m5.large-perftest"
+    Name = "virginia-micro"
 
   }
 }
@@ -77,19 +98,25 @@ resource "aws_spot_instance_request" "virginia-small" {
 resource "aws_spot_instance_request" "virginia-large" {
   provider      = aws.aws_USE1
   count         = var.quantities.USE1.large
-  ami           = var.USE1.amis.ubuntu
+  ami           = var.config.USE1.amis.ubuntu_amd
   instance_type = var.instance_types.large
   spot_price    = "0.10"
-  key_name      = var.USE1.key_name
+  key_name      = var.config.USE1.key_name
   # iam_instance_profile = aws_iam_instance_profile.build-profile.name
 
   user_data = file("startup.sh")
   tags = {
-    Name = "virginia-m5.large-perftest"
+    Name = "virginia-m5.large"
 
   }
 }
+
+
 output "instances" {
-  value       = "${aws_spot_instance_request.ohio-large.*.private_ip}"
+  value       = {
+    ohio-large: "${aws_spot_instance_request.ohio-large.*.private_ip}"
+    ohio-micro-amazon: "${aws_spot_instance_request.ohio-micro-amazon.*.private_ip}"
+    ohio-micro-ubuntu: "${aws_spot_instance_request.ohio-micro-ubuntu.*.private_ip}"
+  }
   description = "PrivateIP address details"
 }
--- iac/variable.tf
+++ iac/variable.tf
@@ -1,43 +1,77 @@
-variable "USE2" {
+# Most of these vars should change relatively rarely, perhaps when new AMIs come out.
+# For rapidly changing vars (e.g. per-execution quantities), declare a default here and
+# override in terraform.tfvars, which is not under source control.
+
+# This var could be something like "regional-config" or we can put other kinds of
+# config in here as needed.
+variable "config" {
   default = {
-    name     = "us-east-2"
-    location = "ohio"
-    amis = {
-      amazon_linux = "ami-0103f211a154d64a6"
-# Refreshing 2023-07-31: old is now the previous "arm" which gives warnings about daemons.
-      old_ubuntu       = "ami-0f12014c8b2f26d33"  
-      arm_ubuntu       = "ami-08fdd91d87f63bb09"
-  # To use this on my work computer
-      ubuntu       = "ami-0fa42adc23c516686"  
+    USE2 = {
+      name     = "us-east-2"
+      location = "ohio"
+      amis     = {
+        #        amazon_linux = "ami-0103f211a154d64a6"
+        amazon_linux = "ami-02a89066c48741345"
+        ubuntu_arm   = "ami-0b5801d081fa3a76c"
+        ubuntu_amd   = "ami-05d251e0fc338590c"
+      }
+      key_name = "2023-07-build-ohio"
     }
-    key_name = "2023-07-build-ohio"
+    USE1 = {
+      name     = "us-east-1"
+      location = "virginia"
+      amis     = {
+        amazon_linux = "ami-06e46074ae430fba6"
+        ubuntu_arm   = "ami-067cf009aedb2612d"
+        ubuntu_amd   = "ami-02675d30b814d1daa"
+      }
+      key_name = "2023-virginia-build"
+    }
+    SAE1 = {
+      name     = "sa-east-1"
+      location = "sao-paulo"
+      amis     = {
+        ubuntu_amd = "ami-050eeeeea5805d75b"
+      }
+      key_name = "2023-sao-paulo-build"
+    }
   }
+
 }
 
 variable "USE1" {
-    
-      default = {
+
+  default = {
     name     = "us-east-1"
     location = "virginia"
-    amis = {
+    amis     = {
       amazon_linux = "ami-06e46074ae430fba6"
-      ubuntu       = "ami-09b435c65f97ace2e" 
+      ubuntu       = "ami-09b435c65f97ace2e"
     }
     key_name = "2023-virginia-build"
   }
 }
 
+# Reminder -- these are overridden in terraform.tfvars. This is intended simply as a default.
 variable "quantities" {
   default = {
-    USE2 = { small = 0, large = 0 }
-    USE1 = { small = 0, large = 0 }
-    SAE1 = { small = 0, large = 0 }
+    USE2 = {
+      micro = { amazon = 0, ubuntu = 0 },
+      large = 0
+    }
+    USE1 = { micro = 0, large = 0 }
+    SAE1 = { micro = 0, large = 0 }
   }
 }
 
 variable "instance_types" {
   default = {
-    small = "t2.micro"
+    micro = "t2.micro"
     large = "m5.large"
   }
 }
+
+# For archival purposes only
+
+# Used from Q1 or 2 2023 until 2023-07
+# old_ubuntu       = "ami-0f12014c8b2f26d33"  
