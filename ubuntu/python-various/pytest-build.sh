d38c64f613e14fe7178f1585158c1036a5c09fc8
Philip J. Reed <reed9999@uw.edu>
2023-08-05 02:37:48 +0000

    clean up pytest; use py3.10 venv

--- ubuntu/python-various/pytest-build.sh
+++ ubuntu/python-various/pytest-build.sh
@@ -2,14 +2,25 @@ echo "##### PYTEST"
 echo "pytest-build start time $(date)"
 
 source ~/scripts/util/detect-installer.sh
-echo $BVT_DISTRO
-echo $BVT_INSTALLER
+echo "Linux distribution: $BVT_DISTRO"
+echo "Selected installer: $BVT_INSTALLER"
 
 pushd ~
 git clone https://github.com/pytest-dev/pytest.git
 cd ~/pytest
-# sudo $BVT_INSTALLER install -y python3.8-venv
-sudo $BVT_INSTALLER install -y python3.11-venv
+# sudo $BVT_INSTALLER install -y python3.11-venv
+# The message says 3.10 -- but why???
+sudo apt install -y python3.10-venv
+
+echo "*****
+python3.11-venv is installed by root
+Err, I mean 3.10
+ready to load venv and start building.
+*****
+
+
+
+"
 python3 -m venv ~/.virtualenvs/pytestdev
 source ~/.virtualenvs/pytestdev/bin/activate
 python3 -m pip install --editable .
