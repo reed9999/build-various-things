0f5d9da190ba8254f4bbc136df1054c13191b0e4
Philip J. Reed <philip.reed@icf.com>
2023-08-07 19:01:53 -0400

    attempt fix to ubuntu django build
    Not sure where this went wrong -- apparently at some
    point we stopped activating the venv!

--- ubuntu/python-various/django-build.sh
+++ ubuntu/python-various/django-build.sh
@@ -5,21 +5,26 @@
 # sudo ln -s /usr/bin/python3.11 /usr/bin/python3
 git clone --depth 1 https://github.com/django/django.git
 pushd ~/django
-sudo apt install python3.11-venv
-rm -rf ~/.virtualenvs/djangodev
-python3.11 -m venv ~/.virtualenvs/djangodev
-    python3 -m pip install --editable .
-    python3 -m pip install tblib		# Without this, the test suite might crash on a failure.
-    python3 -m pip install --editable .
+  sudo apt install python3.11-venv
+  rm -rf ~/.virtualenvs/djangodev
 
-    pushd tests
-        python -m pip install -r requirements/py3.txt
-        echo 
-        echo "***** BEGIN DJANGO TESTS $(date)"
-        ./runtests.py
-        echo "***** END DJANGO TESTS $(date)"
-        echo
-    popd  # from tests
-deactivate
-popd  # from django
+  # Possibly needed to fix KeyError: 'unix_user'
+  # https://github.com/pypa/setuptools/issues/2938#issuecomment-1239769624
+  sudo apt purge python3-setuptools
 
+  python3.11 -m venv ~/.virtualenvs/djangodev
+  source ~/.virtualenvs/djangodev/bin/activate
+  python3 -m pip install --editable .
+  python3 -m pip install tblib # Without this, the test suite might crash on a failure.
+  python3 -m pip install --editable .
+
+  pushd tests
+    python -m pip install -r requirements/py3.txt
+    echo
+    echo "***** BEGIN DJANGO TESTS $(date)"
+    ./runtests.py
+    echo "***** END DJANGO TESTS $(date)"
+    echo
+  popd # from tests
+  deactivate
+popd # from django
