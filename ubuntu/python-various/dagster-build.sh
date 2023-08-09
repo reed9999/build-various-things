c3795effc354af025362b2dcac74445c0396256d
Philip J. Reed <philip.reed@icf.com>
2023-08-08 21:18:37 -0400

    attempt dagster fix/automation
    This seems to fail but can *SORT OF* be
    ignored... however, the tests were then failing.
    Let's see if that's a consistent problem.

--- ubuntu/python-various/dagster-build.sh
+++ ubuntu/python-various/dagster-build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# On Ubuntu ec2, not Azure
-# Remember I also have a Mac version of this on my work Mac.
+# Under development on Ubuntu on EC2
+# See https://docs.dagster.io/community/contributing
 
 # I don't get why pushd isn't working under nohup
 cd ~
-sudo apt update         # critical
+sudo apt update # critical
 sudo apt install -y python3.10-venv
 sudo apt install -y python3-pip
 sudo apt install -y nodejs
@@ -14,35 +14,35 @@ rm -rf ~/.virtualenvs/dagsterdev
 python3 -m venv ~/.virtualenvs/dagsterdev
 
 source ~/.virtualenvs/dagsterdev/bin/activate
-echo "post-activate. which python? $(which python3)"
+  echo "post-activate. which python? $(which python3)"
 
-    echo "Is node installed? $(node -v)"
-    echo
-    echo
+  echo "Is node installed? $(node -v)"
+  echo
 
-    # I should really set up my ssh, but I haven't yet.
-    # git clone git@github.com:dagster-io/dagster.git
-    git clone --depth=100 https://github.com/dagster-io/dagster.git
-        cd ~/dagster
-        echo "after cd to dagster pwd is $(pwd) at $(date)"
-        python3 -m pip install --editable .
-	echo "just before make, our python lives at $(which python3)"
-	echo "just before make, $(date)"
-    echo
-    echo
-        make dev_install
-	echo "just after make, $(date)"
+  # I should really set up my ssh, but I haven't yet.
+  # git clone git@github.com:dagster-io/dagster.git
+  git clone --depth=20 https://github.com/dagster-io/dagster.git
+  cd ~/dagster
+  echo "after cd to dagster pwd is $(pwd) at $(date)"
 
-        python3 -m pytest python_modules/dagster/dagster_tests
-	echo "just after pytest, $(date)"
+# I can't tell if this is called for here or if I just adapted it from another build
+#  python3 -m pip install --editable .
 
-        popd
+  echo "just before make, our python lives at $(which python3)"
+  echo "just before make, $(date)"
+  echo
+  make dev_install || echo <<HERE
+***** make dev install failed *****
+
+HERE
+  echo "just after make, $(date)"
+
+  python3 -m pytest python_modules/dagster/dagster_tests
+  echo "just after pytest, $(date)"
+
 deactivate
-popd
 
-
-
-EC2PROBLEMS=<<-HERE
+EC2PROBLEMS= <<-HERE
 No VM guests are running outdated hypervisor (qemu) binaries on this host.
 fatal: destination path 'dagster' already exists and is not an empty directory.
 ~/dagster ~ ~
