c1394c5f372e186f6049c5bb2b915c4c19d09ddc
Philip J. Reed <philip.reed@icf.com>
2023-09-01 13:36:12 -0400

    add haskell fixes; still not 100% work

--- ubuntu/haskell.sh
+++ ubuntu/haskell.sh
@@ -25,7 +25,19 @@ pushd ~/ghc
 ./boot && ./configure
 
 # Ignore the bootstrap objection for now -- install Haskell to build Haskell
-sudo ${BVT_INSTALLER} install -y ghc
+if [[ "$BVT_DISTRO" == "ubuntu" ]]; then
+  sudo add-apt-repository universe
+  export BOOTSTRAP_HASKELL_NONINTERACTIVE=1
+  export BOOTSTRAP_HASKELL_MINIMAL=1
+  export BOOTSTRAP_HASKELL_VERBOSE=1
+  export BOOTSTRAP_HASKELL_GHC_VERSION=9.4.4
+# I think the message to "ensure" the following was informational, not an error
+#  sudo ${BVT_INSTALLER} install -y build-essential curl libffi-dev libffi8ubuntu1 libgmp-dev libgmp10 \
+#    libncurses-dev libncurses5 libtinfo5
+
+  curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
+fi
+sudo ${BVT_INSTALLER} install -y ghc=9.4
 sudo ${BVT_INSTALLER} install -y cabal-install
 cabal update
 hadrian/build -j
